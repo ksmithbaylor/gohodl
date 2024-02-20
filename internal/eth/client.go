9e859ee2dcae3c43f06194a3d2263c1ae77c8544
Kevin Smith <ksmithbaylor@gmail.com>
2024-02-19 20:20:48 -0500

    Add symbol/decimal cache to eth client, check chain id

--- internal/eth/client.go
+++ internal/eth/client.go
@@ -10,19 +10,28 @@ import (
 )
 
 type Client struct {
-	Network types.EthNetwork
+	Network types.EthNetwork // The network the client is for
 
-	connections map[string]*ethclient.Client
+	connections  map[string]*ethclient.Client // Maps RPC URL to the corresponding eth client
+	symbolCache  map[string]string            // Caches token contract `symbol()` lookups
+	decimalCache map[string]uint8             // Caches token contract `decimals()` lookups
 }
 
 func NewClient(network types.EthNetwork) (*Client, error) {
 	connections := make(map[string]*ethclient.Client, 0)
+	symbolCache := make(map[string]string, 0)
+	decimalCache := make(map[string]uint8, 0)
 
 	for _, rpc := range network.Config.RPCs {
 		client, err := ethclient.Dial(rpc)
 		if err == nil {
-			connections[rpc] = client
-			util.Debugf("Connected to %s\n", rpc)
+			chainID, err := client.ChainID(context.Background())
+			if err == nil && chainID != nil {
+				if chainID.Int64() == int64(network.Config.ChainID) {
+					connections[rpc] = client
+					util.Debugf("Connected to %s\n", rpc)
+				}
+			}
 		}
 	}
 
@@ -31,8 +40,10 @@ func NewClient(network types.EthNetwork) (*Client, error) {
 	}
 
 	return &Client{
-		Network:     network,
-		connections: connections,
+		Network:      network,
+		connections:  connections,
+		symbolCache:  symbolCache,
+		decimalCache: decimalCache,
 	}, nil
 }
 
