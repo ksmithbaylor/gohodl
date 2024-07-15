d5afb8c7c6b426afb4bcae61f2986c2cf3d1dd82
Kevin Smith <ksmithbaylor@gmail.com>
2024-07-15 17:52:37 -0400

    Start on refactoring evm calls to be more generic

--- internal/evm/calls.go
+++ internal/evm/calls.go
@@ -1,10 +1,45 @@
 package evm
 
 import (
+	"fmt"
+
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 )
 
+func ethCall(to common.Address, sig string, args ...any) (ethereum.CallMsg, error) {
+	selector, err := encodeSelector(sig)
+	if err != nil {
+		return ethereum.CallMsg{}, fmt.Errorf("Unable to encode selector for signature '%s': %w", sig, err)
+	}
+
+	encodedArgs, err := encodeArgs(sig, args)
+	if err != nil {
+		return ethereum.CallMsg{}, fmt.Errorf("Unable to encode args '%v': %w", args, err)
+	}
+
+	data := make([]byte, 0, len(selector)+len(encodedArgs))
+	data = append(data, selector...)
+	data = append(data, encodedArgs...)
+
+	return ethereum.CallMsg{
+		To:   &to,
+		Data: data,
+	}, nil
+}
+
+func encodeSelector(sig string) ([]byte, error) {
+	// TODO use something from geth to encode the signature to the 4-byte selector
+	return make([]byte, 0), nil
+}
+
+func encodeArgs(sig string, args []any) ([]byte, error) {
+	// TODO use something from geth to encode the arguments
+	return make([]byte, 0), nil
+}
+
+// TODO: replace with something like:
+// msg, err := ethCall(contract, "decimals()")
 func decimalsCall(to common.Address) ethereum.CallMsg {
 	return ethereum.CallMsg{
 		To:   &to,
@@ -12,6 +47,8 @@ func decimalsCall(to common.Address) ethereum.CallMsg {
 	}
 }
 
+// TODO: replace with something like:
+// msg, err := ethCall(contract, "symbol()")
 func symbolCall(to common.Address) ethereum.CallMsg {
 	return ethereum.CallMsg{
 		To:   &to,
@@ -19,6 +56,8 @@ func symbolCall(to common.Address) ethereum.CallMsg {
 	}
 }
 
+// TODO: replace with something like:
+// msg, err := ethCall(contract, "balanceOf(address)", addr)
 func balanceCall(to common.Address, a common.Address) ethereum.CallMsg {
 	data := make([]byte, 0, 36)
 
