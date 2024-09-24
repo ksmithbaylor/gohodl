73d4fb157fd8f8f4288a75e2bf73a6f988912099
Kevin Smith <ksmithbaylor@gmail.com>
2024-09-24 17:09:44 -0400

    Show progress for unhandled transactions

--- internal/ctc/4_export.go
+++ internal/ctc/4_export.go
@@ -125,7 +125,7 @@ func ExportTransactions(db *util.FileDB, clients generic.AllNodeClients) {
 	}
 
 	fmt.Printf("%d transactions handled out of %d (%.2f%%)\n", handledTxs, totalTxs, 100.0*float32(handledTxs)/float32(totalTxs))
-	fmt.Printf("%d transactions temporarily not handled\n", unhandled)
+	fmt.Printf("%d transactions temporarily not handled (will be %.2f%% when done)\n", unhandled, 100.0*float32(handledTxs+unhandled)/float32(totalTxs))
 	fmt.Println("Finished reading txs from CSV!")
 }
 
