5fdda99fd96e75e2239b2592ea9e91d413e8714d
Kevin Smith <ksmithbaylor@gmail.com>
2024-10-13 13:14:14 -0400

    Add printing to template since that's always the first step

--- internal/handlers/kevin/template.go
+++ internal/handlers/kevin/template.go
@@ -2,10 +2,17 @@ package kevin
 
 import (
 	"github.com/ksmithbaylor/gohodl/internal/evm"
+	"github.com/ksmithbaylor/gohodl/internal/evm_util"
 	"github.com/ksmithbaylor/gohodl/internal/handlers"
 )
 
 // Rename this to `handleWhatever` for each new type of transaction
 func HandlerTemplate(bundle handlers.TransactionBundle, client *evm.Client, export handlers.CTCWriter) error {
+	printHeader(bundle)
+	netTransfers, err := evm_util.NetTokenTransfersOnlyMine(client, bundle.Info, bundle.Receipt.Logs)
+	if err != nil {
+		return err
+	}
+	netTransfers.Print()
 	return NOT_HANDLED
 }
