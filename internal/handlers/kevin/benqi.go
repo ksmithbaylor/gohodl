733a754bf908cd7455bf85423ddce30fa5febb40
Kevin Smith <ksmithbaylor@gmail.com>
2024-10-13 10:40:18 -0400

    Handle more benqi txs

--- internal/handlers/kevin/benqi.go
+++ internal/handlers/kevin/benqi.go
@@ -16,6 +16,7 @@ var BENQI_CONTRACTS = []string{
 	"0xc9e5999b8e75C3fEB117F6f73E664b9f3C8ca65C",
 	"0x35Bd6aedA81a7E5FC7A7832490e71F757b0cD9Ce",
 	"0xBEb5d47A3f720Ec0a390d04b4d41ED7d9688bC7F",
+	"0x755c78D3bC25e297e8E010A2D1FCf49Cc43ADa21",
 }
 
 func handleBenqi(bundle handlers.TransactionBundle, client *evm.Client, export handlers.CTCWriter) error {
@@ -25,20 +26,23 @@ func handleBenqi(bundle handlers.TransactionBundle, client *evm.Client, export h
 	}
 
 	switch bundle.Info.Method {
-	case "0xa0712d68": // mint(uint256)
+	case "0xa0712d68", "0xb6b55f25": // mint(uint256), deposit(uint256)
 		return handleBenqiMint(bundle, client, export, netTransfers)
 	case "0xc5ebeaec": // borrow(uint256)
 		return handleBenqiBorrow(bundle, client, export, netTransfers)
 	case "0x0e752702": // repayBorrow(uint256)
 		return handleBenqiRepay(bundle, client, export, netTransfers)
-	case "0x852a12e3": // redeemUnderlying(uint256)
+	case "0x852a12e3", "0x2e1a7d4d": // redeemUnderlying(uint256), withdraw(uint256)
 		return handleBenqiRedeem(bundle, client, export, netTransfers)
+	case "0xfdb5a03e": // reinvest()
+		// Not quite accurate, but close enough
+		return handleBenqiClaimRewards(bundle, client, export, netTransfers)
 	}
 
 	return NOT_HANDLED
 }
 
-// The below is mostly copied from the moonwell handlers
+// The below is mostly copied from the moonwell/aave handlers
 
 func handleBenqiMint(bundle handlers.TransactionBundle, _ *evm.Client, export handlers.CTCWriter, netTransfers evm_util.NetTransfers) error {
 	if len(netTransfers) != 2 {
@@ -198,6 +202,48 @@ func handleBenqiRedeem(bundle handlers.TransactionBundle, _ *evm.Client, export 
 		From:         "benqi",
 		To:           bundle.Info.From,
 		Description:  fmt.Sprintf("benqi: withdraw %s", withdrawn),
+	}
+	ctcTx.AddTransactionFeeIfMine(bundle.Info.From, bundle.Info.Network, bundle.Receipt)
+
+	return export(ctcTx.ToCSV())
+}
+
+func handleBenqiClaimRewards(bundle handlers.TransactionBundle, client *evm.Client, export handlers.CTCWriter, netTransfers evm_util.NetTransfers) error {
+	if len(netTransfers) != 1 {
+		panic("Unexpected net transfers for benqi claim rewards")
+	}
+
+	var claimed core.Amount
+
+	for _, transfers := range netTransfers {
+		if len(transfers) != 1 {
+			panic("Unexpected net transfers for benqi claim rewards")
+		}
+		for addr, amount := range transfers {
+			if addr.Hex() != bundle.Info.From {
+				panic("Unexpected net transfers for benqi claim rewards")
+			}
+			if !amount.Value.IsPositive() {
+				panic("Outflow for benqi claim rewards")
+			}
+			claimed = *amount
+		}
+	}
+
+	if claimed.Asset.Symbol == "" {
+		panic("Nothing claimed for benqi claim rewards")
+	}
+
+	ctcTx := ctc_util.CTCTransaction{
+		Timestamp:    time.Unix(int64(bundle.Block.Time), 0).UTC(),
+		Blockchain:   bundle.Info.Network,
+		ID:           bundle.Info.Hash,
+		Type:         ctc_util.CTCIncome,
+		BaseCurrency: claimed.Asset.Symbol,
+		BaseAmount:   claimed.Value,
+		From:         "benqi",
+		To:           bundle.Info.From,
+		Description:  fmt.Sprintf("benqi: claim %s in rewards", claimed),
 	}
 	ctcTx.AddTransactionFeeIfMine(bundle.Info.From, bundle.Info.Network, bundle.Receipt)
 
