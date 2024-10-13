1f40287fa301dd4851f9e0608f51be1791d54efc
Kevin Smith <ksmithbaylor@gmail.com>
2024-10-13 10:37:39 -0400

    Temporarily only analyze methods for easier workflow

--- scripts/analyze.sql
+++ scripts/analyze.sql
@@ -27,55 +27,55 @@ create temp view unique_methods as
   group by method
   order by how_many desc;
 
-select count(*) as 'unique methods' from unique_methods;
 select
   method,
   how_many,
   destinations,
-  sum(how_many) over (order by how_many desc rows unbounded preceding) as cumulative
+  sum(how_many) over (order by how_many desc, method rows unbounded preceding) as cumulative
 from unique_methods
-limit 20;
+order by how_many, method desc;
+select count(*) as 'unique methods' from unique_methods;
 
-create temp view unique_destinations as
-  select
-    network,
-    "to",
-    count(*) as how_many,
-    count(distinct method) as methods
-  from txs
-  where txs.hash not in (select substr("ID (Optional)", 0, 67) from ctc)
-    and txs.timestamp <= 1704067199
-  group by network, "to"
-  order by how_many desc;
-
-select count(*) as 'unique destinations' from unique_destinations;
-select
-  network,
-  "to",
-  how_many,
-  methods,
-  sum(how_many) over (order by how_many desc rows unbounded preceding) as cumulative
-from unique_destinations
-limit 20;
-
-create temp view unique_calls as
-  select
-    network,
-    "to",
-    method,
-    count(*) as how_many
-  from txs
-  where txs.hash not in (select substr("ID (Optional)", 0, 67) from ctc)
-    and txs.timestamp <= 1704067199
-  group by network, "to", method
-  order by how_many desc;
-
-select count(*) as 'unique calls' from unique_calls;
-select
-  network,
-  "to",
-  method,
-  how_many,
-  sum(how_many) over (order by how_many desc rows unbounded preceding) as cumulative
-from unique_calls
-limit 50;
+-- create temp view unique_destinations as
+--   select
+--     network,
+--     "to",
+--     count(*) as how_many,
+--     count(distinct method) as methods
+--   from txs
+--   where txs.hash not in (select substr("ID (Optional)", 0, 67) from ctc)
+--     and txs.timestamp <= 1704067199
+--   group by network, "to"
+--   order by how_many desc;
+--
+-- select count(*) as 'unique destinations' from unique_destinations;
+-- select
+--   network,
+--   "to",
+--   how_many,
+--   methods,
+--   sum(how_many) over (order by how_many desc rows unbounded preceding) as cumulative
+-- from unique_destinations
+-- limit 20;
+--
+-- create temp view unique_calls as
+--   select
+--     network,
+--     "to",
+--     method,
+--     count(*) as how_many
+--   from txs
+--   where txs.hash not in (select substr("ID (Optional)", 0, 67) from ctc)
+--     and txs.timestamp <= 1704067199
+--   group by network, "to", method
+--   order by how_many desc;
+--
+-- select count(*) as 'unique calls' from unique_calls;
+-- select
+--   network,
+--   "to",
+--   method,
+--   how_many,
+--   sum(how_many) over (order by how_many desc rows unbounded preceding) as cumulative
+-- from unique_calls
+-- limit 50;
