0768278148f27f37b78a694b50784908270bbb0e
Kevin Smith <ksmithbaylor@gmail.com>
2024-10-14 15:02:05 -0400

    Handle solarflare LP ops

--- internal/handlers/kevin/handle_transaction.go
+++ internal/handlers/kevin/handle_transaction.go
@@ -183,6 +183,7 @@ func (h personalHandler) HandleTransaction(
 	case info.Method == "0xde5f6268":
 		handle = handleMiscWithLabel("deposit lp token into beefy or similar")
 	case
+		info.Method == "0x515bc323",
 		info.Method == "0xf3fef3a3",
 		info.Method == "0xa0712d68",
 		info.Method == "0x9dbb844d",
