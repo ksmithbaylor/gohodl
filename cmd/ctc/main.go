58e1660e338650286a568580e5a3f8ef9efbd7f9
Kevin Smith <ksmithbaylor@gmail.com>
2025-12-27 21:33:46 -0500

    Remove extra env var

--- cmd/ctc/main.go
+++ cmd/ctc/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"os"
-
 	"github.com/ksmithbaylor/gohodl/internal/config"
 	"github.com/ksmithbaylor/gohodl/internal/ctc"
 	"github.com/ksmithbaylor/gohodl/internal/generic"
@@ -16,10 +14,6 @@ func main() {
 	clients := generic.NewAllNodeClients(cfg.AllNetworks())
 
 	ctc.IdentifyTransactions(db, clients)
-	if _, ok := os.LookupEnv("STOP_AFTER_IDENTIFY"); ok {
-		return
-	}
-
 	txHashes := ctc.FetchTransactions(db, clients)
 	ctc.AnalyzeTransactions(db, txHashes)
 	ctc.ExportTransactions(db, clients)
