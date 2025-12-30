c2ecd96a129a1ac01ec475c26c1f7524579af3ee
Kevin Smith <ksmithbaylor@gmail.com>
2025-12-30 15:43:51 -0500

    Add moonwell governance, 65.16%

--- internal/handlers/kevin/handle_transaction.go
+++ internal/handlers/kevin/handle_transaction.go
@@ -167,8 +167,8 @@ func (h personalHandler) HandleTransaction(
 		handle = handleFriendTechSell
 	case info.Method == "0x52c7f8dc":
 		handle = handleRewardWithLabel("XEN Crypto")
-		// case info.Method == "0x56781388":
-		//   handle = handleMiscWithLabel("moonwell governance vote")
+	case info.Method == "0x56781388":
+		handle = handleMiscWithLabel("moonwell governance vote")
 		// case info.Method == "0x2046d075":
 		//   handle = handleRewardWithLabel("misc reward")
 		// case info.Method == "0xac9650d8": // multicall
